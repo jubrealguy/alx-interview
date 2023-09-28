5a01febcc65516d72df4f7ba440f7c785a6fe21c
jubrealguy <whizkidjubrealguy@gmail.com>
2023-09-28 23:51:01 +0100

    second commit

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
-
 def pascal_triangle(n):
-    arr = [1, 2, 1]
+    result = []
     for i in range(n):
-        newArr = []
-        newArr.append(1)
-        for j in range(len(arr) - 1):
-            newArr[j + 1] = arr[j] + arr[j + 1]
-        newArr.insert(-1, 1)
-        newArr = arr
-        return newArr
+        if i == 0:
+            result.append([1])  # First row: [1]
+        else:
+            # Calculate the current row based on the previous row
+            prev_row = result[-1]
+            new_row = [1]
+            for j in range(1, len(prev_row)):
+                new_row.append(prev_row[j - 1] + prev_row[j])
+            new_row.append(1)
+            result.append(new_row)
+    return result
