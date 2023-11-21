3d2aa6797c9c912c1722d4f70be26ea8fd3c3877
jubrealguy <whizkidjubrealguy@gmail.com>
2023-11-21 20:28:46 +0100

    lockboxes solved

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,14 +1,17 @@
 #!/usr/bin/python3
-
-"""A Lockboxes algorithm"""
+"""Script will unlock list of lists"""
 
 
 def canUnlockAll(boxes):
-    """A function  that determines if all the boxes can be opened."""
-    for i in range(1, len(boxes)):
-        others = boxes[:i] + boxes[i + 1:]
-        for j in range(len(others)):
-            for k in range(len(others[j])):
-                if boxes[j][k] == i and i == j:
-                    return True
-            return False;
+    """This function will take a list of lists and the content
+       of a list will unlock other lists
+    """
+
+    keys = [0]
+    for key in keys:
+        for boxKey in boxes[key]:
+            if boxKey not in keys and boxKey < len(boxes):
+                keys.append(boxKey)
+    if len(keys) == len(boxes):
+        return True
+    return False
