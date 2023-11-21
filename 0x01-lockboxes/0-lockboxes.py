7fa2d1bbc3839d50f0c369744022d6cd2cb7c8f7
jubrealguy <whizkidjubrealguy@gmail.com>
2023-11-21 19:57:18 +0100

    added semi-colon

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -5,10 +5,10 @@
 
 def canUnlockAll(boxes):
     """A function  that determines if all the boxes can be opened."""
-    for i in range(len(boxes)):
+    for i in range(1, len(boxes)):
         others = boxes[:i] + boxes[i + 1:]
         for j in range(len(others)):
             for k in range(len(others[j])):
-                if boxes[j][k] == i:
+                if boxes[j][k] == i and i == j:
                     return True
-    return False
+            return False;
